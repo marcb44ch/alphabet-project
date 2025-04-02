057d8c1ac0a69ceb6fc891f60a1c7b76d878f2bb
Marc Bach <marc.bach.padros@uvic.cat>
2025-04-02 18:41:05 +0200

    Add ECharts integration: Implement Chart component and update AboutView to display a line chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import * as echarts from 'echarts'
+import VChart, { THEME_KEY } from 'vue-echarts'
+import { provide } from 'vue'
+import {
+    TitleComponent,
+    TooltipComponent,
+    ToolboxComponent,
+    LegendComponent,
+    GridComponent,
+    DataZoomComponent
+} from 'echarts/components'
+import { BarChart, PieChart, LineChart } from 'echarts/charts'
+
+provide(THEME_KEY, 'dark')
+const props = defineProps({
+    options : {type: Object}
+})
+
+use([
+    CanvasRenderer,
+    BarChart,
+    PieChart,
+    LineChart,
+    TitleComponent,
+    TooltipComponent,
+    ToolboxComponent,
+    LegendComponent,
+    GridComponent,
+    DataZoomComponent,
+])
+</script>
+
+<template>
+  <v-chart :option="props.options" autoresize></v-chart>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,15 +13,32 @@
     {nom: "Anna", cognom: "Casas", adreça: "Carrer Nou 1", telefon: "666666666"},
     {nom: "Anna", cognom: "Cussó", adreça: "Carrer Nou 1", telefon: "666666666"}
   ])
+  import Chart from '@/components/Chart.vue'
+  const options = ref({
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  })
 </script>
 
 <template>
   
-  <div class="about">
+  <div class="about" style="height: 400px;">
     <h1>This is an about page</h1>
     Hola taula de prova
     <Table :headers="headers" :items="items"></Table>
-
+    <chart :options="options">
+    </chart>
   </div>
 </template>
 
